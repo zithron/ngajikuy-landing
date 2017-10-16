cbc211672f5767724beab5ee8c2c57a9817803f4
zithron <abdulbasithb70@gmail.com>
2017-10-16 21:32:45 +0700

    edit section again

--- js/semnas.js
+++ js/semnas.js
@@ -2,7 +2,7 @@ $(window).scroll(function() {
     
     var wScroll = $(this).scrollTop();
     
-    if(wScroll > $('#speakers').offset().top) {
+    if(wScroll > $('#feature').offset().top) {
         $('#navbar').addClass('navbar-fixed');
     } else {
         $('#navbar').removeClass('navbar-fixed');
