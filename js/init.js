05f919303cf18402a3191a5fafbf434e31416757
zithron <abdulbasithb70@gmail.com>
2017-10-24 01:36:11 +0700

    push to gh pages

--- js/init.js
+++ js/init.js
@@ -1,11 +1,26 @@
-(function ($) {
-    $(function () {
+(function($) {
+    $(function() {
 
         $('.button-collapse').sideNav();
         $('.parallax').parallax();
         $('.collapsible').collapsible();
         $('select').material_select();
-        $('.modal').modal();
+        // $('.modal').modal();
+
+
+        var url = $("#teaser").attr('src');
+
+        $("#teaser").attr('src', '');
+
+        $('.modal').modal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+                $("#teaser").attr('src', url);
+            },
+            complete: function() { // Callback for Modal close
+                $("#teaser").attr('src', '');
+            }
+        });
 
 
     }); // end of document ready
